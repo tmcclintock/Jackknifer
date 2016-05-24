32b13c171af1a4cb609a8b242b1249bae9083154
Tom McClintock <tmcclintock@email.arizona.edu>
2016-05-23 17:29:00 -0700

    Renamed everything to setup_jackknifer because it was misleading.

--- setup_jackknife.py
+++ setup_jackknife.py
@@ -1,25 +1,26 @@
 """
-This file contains the jackknifer class that extends object. The class is
-initiated with a path to a file and a filename, and possibly a number of jackknife regions
+This file contains the setup_jackknifer class that extends object. The class is
+initiated with a path to a file and a filename, and possibly a number of setup_jackknife regions
 which can be changed later. It contains a class method that set up
-jackknife regions according to the locations of the particles in the
+setup_jackknife regions according to the locations of the particles in the
 file provided, as well as a method to scan for the minimum and maximum of
 the regions.
 
-The only dependency is the os modeule, used to make a directory that holds the jackknifes.
+The only dependency is the os modeule, used to make a directory that holds the setup_jackknifes.
 
 This will only give equal volume jkregions for rectangular regions. It can
 operate on non rectangualr regions, but the jkregions will not be equal in volume.
 """
 
 import os
+import sys
 
-class Jackknifer(object):
+class Setup_Jackknifer(object):
     def __init__(self,path,filename,columns=[0,1,2],N=2):
         self.path = path #Path to the file
-        self.filename = filename #Filename to be jackknifed
+        self.filename = filename #Filename to be setup_jackknifed
         self.columns = columns #Which columns in the file contain the position information
-        self.N = N #Number of jackknife regions
+        self.N = N #Number of setup_jackknife regions
         self.measured = False #A flag to tell if the file has been scanned yet
 
     """
@@ -55,15 +56,15 @@ class Jackknifer(object):
         self.maximums = [xmax+xoff,ymax+yoff,zmax+zoff]
     
     """
-    A method to set the number of jackknife regions to a new value.
+    A method to set the number of setup_jackknife regions to a new value.
     """
     def set_N_regions(self,N):
         self.N = N
 
     """
-    Set up the actual jackknife regions.
+    Set up the actual setup_jackknife regions.
     """
-    def jackknife(self):
+    def setup_jackknife(self):
         if not self.measured:
             self.measure()
             self.measured = True
@@ -73,20 +74,20 @@ class Jackknifer(object):
         N = self.N
         xstep,ystep,zstep = (xmax-xmin)/N,(ymax-ymin)/N,(zmax-zmin)/N
 
-        #Make a directory for the new jackknife files
-        jkpath = self.path+"/jackknife_files/"
+        #Make a directory for the new setup_jackknife files
+        jkpath = self.path+"/jackknife_regions/"
         os.system("mkdir "+jkpath)
         filename = self.filename
         infile = open(self.path+"/"+filename,"r")
 
-        #Create the files for each jackknife region
+        #Create the files for each setup_jackknife region
         filelist = []
         for i in range(N**3):
             outfile = open(jkpath+"jk%i_"%i+filename,"w")
             filelist.append(outfile)
         
         #Loop over the lines in the input file and put them
-        #in the correct jackknife file
+        #in the correct setup_jackknife file
         i = 0
         for line in infile:
             if line[0]=='#':
@@ -102,13 +103,13 @@ class Jackknifer(object):
             filelist[i].close()
 
 """
-A unit test for creating some jackknife regions
+A unit test for creating some setup_jackknife regions
 """
 if __name__ == '__main__':
-    jkr = Jackknifer(".","test.txt")
+    jkr = Setup_Jackknifer(".","test.txt")
     jkr.measure()
     print jkr.minimums, jkr.maximums
-    jkr.jackknife()
+    jkr.setup_jackknife()
 
-    #myjkr = Jackknifer(path="/media/tom/Data/Emulator_data/reduced_data",filename="reduced_box000_Z9.list",columns=[8,9,10],N=8)
-    #myjkr.jackknife()
+    #myjkr = Setup_Jackknifer(path="/media/tom/Data/Emulator_data/reduced_data",filename="reduced_box000_Z9.list",columns=[8,9,10],N=8)
+    #myjkr.setup_jackknife()
